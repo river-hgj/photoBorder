77afb6a186066ff0b1c9c404a6be257c1b6cc483
Pylogmon <pylogmon@outlook.com>
2026-05-09 20:48:49 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const getBasePath = () => {
-  if (!process.env.GITHUB_ACTIONS) {
-    return '/'
-  }
-
-  const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-
-  if (!repositoryName || repositoryName.endsWith('.github.io')) {
-    return '/'
-  }
-
-  return `/${repositoryName}/`
-}
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: getBasePath(),
+  base: '/',
   plugins: [react()],
 })

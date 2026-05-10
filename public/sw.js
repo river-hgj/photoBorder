32a9b3c0810d25c268f007ae9a2305891026beef
Pylogmon <pylogmon@outlook.com>
2026-05-10 09:51:19 +0800

    Update PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'photo-border-v3'
+const CACHE_NAME = 'photo-border-v4'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/app-icon.png', '/icon-192.png', '/icon-512.png']
 
 self.addEventListener('install', (event) => {
